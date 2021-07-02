d90ff67e29c4b2b5cacd29bbf9be43ebffdcf17a
myjjustin <78374553+myjJustin@users.noreply.github.com>
2021-07-02 22:40:59 +0200

    init

---
+++ aufgabe 2/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace aufgabe_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(84, 263);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(290, 263);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(496, 263);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ aufgabe 2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Timers;
+
+namespace aufgabe_2
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("iowkit", SetLastError = true)]
+        public static extern int IowKitOpenDevice();
+
+        [DllImport("iowkit", SetLastError = true)]
+        public static extern void IowKitCloseDevice(int iowHandle);
+
+        [DllImport("iowkit", SetLastError = true)]
+        public static extern void IowKitWrite(int iowHandle, int numPipe, ref byte buffer, int length);
+
+        [DllImport("iowkit", SetLastError = true)]
+        private static extern int IowKitGetDeviceHandle(int numDevice);
+
+        [DllImport("iowkit", SetLastError = true)]
+        private static extern int IowKitReadNonBlocking(int iowHandle, int numPipe, ref byte buffer, int length);
+
+        public int newByte;
+
+        public int handle;
+        public System.Timers.Timer aTimer = new System.Timers.Timer();
+        public byte[] data = new byte[5];
+
+        //Buttons
+        private const byte pin1 = 1 << 5;
+        private const byte pin2 = 1 << 6;
+        private const byte pin3 = 1 << 7;
+
+        private const byte allInput = pin1 | pin2 | pin3;
+
+        //grüne LED
+        private const byte pin17 = 1 << 0;
+        //gelbe LED
+        private const byte pin18 = 1 << 1;
+        //rote LED
+        private const byte pin19 = 1 << 2;
+        public Form1()
+        {
+            InitializeComponent();
+            aTimer.Interval = 50;
+            aTimer.Elapsed += TimerElapsed;
+            handle = IowKitOpenDevice();
+            aTimer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            data[0] = 0x00;
+            data[1] = pin1 | pin2 | pin3;
+            data[2] = pin17 | pin18 | pin19;
+            data[3] = 0x00;
+            data[4] = 0x00;
+            IowKitWrite(handle, 0, ref data[0], 5);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            aTimer.Stop();
+            IowKitCloseDevice(handle);
+        }
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+
+            IowKitReadNonBlocking(handle, 0, ref data[0], 5);
+            Console.WriteLine("data = " + string.Join(" ", data));
+            Invoke(new Action(() => {
+                label2.Text = "Data[1] = " + String.Format(" {0:X2} ", data[1]) + " ( " + data[1].ToString() + " ) ";
+            }));
+
+            ChangePColor(panel3, (data[1] & pin1) == 0);
+            ChangePColor(panel2, (data[1] & pin2) == 0);
+            ChangePColor(panel1, (data[1] & pin3) == 0);
+        }
+        void ChangePColor(Panel p, bool active)
+        {
+            if (active)
+            {
+                p.BackColor = Color.Red;
+            }
+            else
+            {
+                p.BackColor = Color.Black;
+            }
+        }
+    }
+}
